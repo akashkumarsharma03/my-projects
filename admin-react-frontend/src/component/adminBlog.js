33a57ef82ca9653a758bb242b035dfd7912b813f
ETEC\AkashKumar <akashkumar@policybazaar.com>
2023-04-11 19:11:58 +0530

    usestate in react

--- admin-react-frontend/src/component/adminBlog.js
+++ admin-react-frontend/src/component/adminBlog.js
@@ -1,10 +1,16 @@
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Form from 'react-bootstrap/Form';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const AdminBlog = () => {
+    const [userData, setUserData] = useState({
+        title: '',
+        category: '',
+        tags: ''
+    })
+
     let fontstyle = {
         color: "white"
     }
@@ -12,25 +18,46 @@ const AdminBlog = () => {
         overflowY: "scroll",
     }
 
+    const handleSubmit = (e) => {
+        debugger;
+        e.preventDefault();
+        alert(userData.title);
+        alert(userData.category);
+        alert(userData.tags);
+
+
+    }
+
+    const handleChange = (event) => {
+        setUserData({ ...userData, [event.target.name]: event.target.value });
+    }
 
     return (
         <>
-            <div className="m-3 p-5 bg-dark">
-                <Form.Group className="mb-3" style={fontstyle}>
-                    <Form.Label>Blog Title</Form.Label>
-                    <Form.Control placeholder="Blog Title" />
-                </Form.Group>
-                <Form.Group className="mb-3" style={fontstyle}>
-                    <Form.Label>Blog Category</Form.Label>
-                    <Form.Select id="categorySelect">
-                        <option> first Category</option>
-                    </Form.Select>
-                </Form.Group>
-                <Form.Group className="mb-3" style={fontstyle}>
-                    <Form.Label>Blog Tags</Form.Label>
-                    <Form.Control placeholder="Blog Tags" />
-                </Form.Group>
-                <Form.Group className="mb-3" style={fontstyle}>
+            <div className="m-3 p-5 bg-dark" >
+                <Form onSubmit={handleSubmit}>
+
+                    <Form.Group className="mb-3" style={fontstyle} >
+                        <Form.Label>Blog Title</Form.Label>
+                        <Form.Control value={userData.title} name="title"
+                            onChange={handleChange} placeholder="Blog Title" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" style={fontstyle}>
+                        <Form.Label>Blog Category</Form.Label>
+                        <Form.Select id="categorySelect" value={userData.category} name="category"
+                            onChange={handleChange}>
+                            <option value={''}>Select One</option>
+                            <option value={'first Category'}> first Category</option>
+                            <option value={'second Category'}> second Category</option>
+
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group className="mb-3" style={fontstyle}>
+                        <Form.Label>Blog Tags</Form.Label>
+                        <Form.Control value={userData.tags} name="tags"
+                            onChange={handleChange} placeholder="Enter Blog Tags" />
+                    </Form.Group>
+                    {/* <Form.Group className="mb-3" style={fontstyle}>
                     <Form.Label>Blog Summary</Form.Label>
                     <Form.Control placeholder="Blog Summary" />
                 </Form.Group>
@@ -42,8 +69,8 @@ const AdminBlog = () => {
                 <Form.Group className="mb-3" style={fontstyle}>
                     <Form.Label>Blog Description</Form.Label>
                     <Form.Control as="textarea" style={scroll} rows={12} placeholder="Blog Description" />
-                </Form.Group>
-                {/* <ReactQuill theme="snow" className="bg-light text-dark w-5"
+                </Form.Group> */}
+                    {/* <ReactQuill theme="snow" className="bg-light text-dark w-5"
                     modules={{
                         toolbar: {
                             container: [
@@ -59,11 +86,13 @@ const AdminBlog = () => {
                         },
                     }}
                     placeholder="Add a description of your event" /> */}
-                    
-                <Form.Group>
-                    <button ClassName="input-group-text shadow-sm btn-primary btn text-white "
-                        id="inputGroup-sizing-sm" style={{ marginTop: "10px" }}>send</button>
-                </Form.Group>
+
+                    <Form.Group>
+                        <button type="submit" ClassName="input-group-text shadow-sm btn-primary btn text-white "
+                            id="inputGroup-sizing-sm" style={{ marginTop: "10px" }}>send</button>
+                    </Form.Group>
+                </Form>
+
             </div>
         </>
     )
--- angular-blog-frontend/crud-app/src/app/diplay/saved/saved.component.ts
+++ angular-blog-frontend/crud-app/src/app/diplay/saved/saved.component.ts
@@ -11,18 +11,38 @@ import { ApiService } from '../../api.service';
 })
 export class SavedComponent implements OnInit {
   apidata: any
+  result: any=[];
+  category: any;
+  isShow: boolean=true;
+  showcate: any=[];
+  filtercategory:  any=[];
   constructor(private ApiService: ApiService) { }
 
   ngOnInit(): void {
     this.ApiService.getdata().subscribe((data) => {
       this.apidata = data
+      
+      for(let i =0; i<=this.apidata.length-1; i++){
+        this.filtercategory.unshift(this.apidata[i].BlogCategory)
+      }
+      this.showcate = [...new Set(this.filtercategory
+        )]
     }, (error) => {
       console.log("An error accessing Service");
     })
+   
   }
 
-  show(){
-
+  show(i: any) {
+    debugger
+    this.isShow = !this.isShow
+    this.ApiService.getdata().subscribe((data) => {
+      this.apidata = data
+      this.category = this.apidata[i].BlogCategory
+      this.result = this.apidata.filter((blog: any) => blog.BlogCategory.includes(this.category));
+    } ,(error) => {
+      console.log("An error accessing Service");
+    })
   }
 }
 
